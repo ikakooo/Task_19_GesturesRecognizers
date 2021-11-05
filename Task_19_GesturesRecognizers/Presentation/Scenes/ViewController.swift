ba0096fef5d365007a41c0cea3d9ec92e1f7621a
ikakooo <i_chkhitunidze@cu.edu.ge>
2021-11-05 15:34:06 +0400

    დასასრული

--- Task_19_GesturesRecognizers/Presentation/Scenes/ViewController.swift
+++ Task_19_GesturesRecognizers/Presentation/Scenes/ViewController.swift
@@ -22,54 +22,37 @@ class MainViewController: UIViewController {
         }
     }
     
+    var lastLocation = CGPoint(x: 0, y: 0)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-       // setupSwipeGesture()
         setupPanGestureForMonkey()
-       // setupPanGestureForBanana()
-       
+    
+        
     }
     @IBAction func onBananaClick(_ sender: UIButton) {
-        let dsfsd = banana.frame.origin.x
-       // for i in Int(banana.frame.origin.x)...Int(UIScreen.main.bounds.height){
-           // sleep(1)
-            //DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(40), execute: {
-               // print("done")
-               /// self.bananaTopArchor.constant = CGFloat(i) - self.banana.frame.origin.x
-                UIView.animate(withDuration: 2) {
-                    self.banana.transform = CGAffineTransform(translationX: 0, y: UIScreen.main.bounds.height - self.monkey.frame.origin.x)
-                    
-                }
-//        UIView.animate(withDuration: 2) {
-//            self.banana.transform = CGAffineTransform(rotationAngle: .pi)
-//        }
-           // })
-//            do {
-//                sleep(1)
-//            }
-            //Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(callbackk), userInfo: nil, repeats: false)
-       // }
-//        let equalitiX = banana.frame.origin.x - monkey.frame.origin.x
-//        if equalitiX
-
+        UIView.animate(withDuration: 3, animations: {
+            self.banana.transform = CGAffineTransform(translationX: 0, y: UIScreen.main.bounds.height - self.monkey.frame.height-100)
+        }, completion: {(finished:Bool) in
+            bananaReturnIfMiss()
+        })
+        
+        
+        func bananaReturnIfMiss(){
+            let equalitiX = self.banana.frame.midX - self.monkey.frame.midX // - self.monkey.frame.origin.x
+           // let equalitiy = self.banana.frame.origin.y - self.monkey.frame.origin.y
+            print(self.monkey.frame.midX)
+            print(equalitiX)
+            if equalitiX * equalitiX < 5000 {
+                print("done")
+                self.banana.isHidden = true
+                self.monkey.image = UIImage(named:"monkeyWithBanana")
+            }
+            else {
+                self.banana.transform = CGAffineTransform(translationX: 0, y: 0)
+            }
+        }
+        
     }
-    
-    
-    @objc private func callbackk() {
-        print("done")
-    }
-
-   
-    
-//    private func setupPanGestureForBanana() {
-//        let fallDown = UISwipeGestureRecognizer(
-//            target: self,
-//            action: #selector(handleDirectedPanGesture(_:))
-//        )
-//
-//        banana.addGestureRecognizer(fallDown)
-//    }
     
     private func setupPanGestureForMonkey() {
         let panGesture = UIPanGestureRecognizer(
@@ -79,7 +62,6 @@ class MainViewController: UIViewController {
         
         monkey.addGestureRecognizer(panGesture)
     }
-
 }
 
 
@@ -91,37 +73,43 @@ private extension MainViewController {
     
     @objc private func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: view)
-        let velocity = gesture.velocity(in: view)
+        // let velocity = gesture.velocity(in: view)
         let width = UIScreen.main.bounds.width
-       // print(translation)
-        print(width)
-        print(monkey.frame.origin.x)
+        // print(translation)
+        //print(width)
+        print(lastLocation)
         if (0 <= monkey.frame.origin.x  ){
             
-            monkey.transform = CGAffineTransform(translationX: translation.x, y: 0)
+            monkey.transform = CGAffineTransform(translationX:   translation.x, y: 0)
+            
             
         }else {
             monkey.transform = CGAffineTransform(translationX: width-1, y: 0)
         }
-        
-//        if (){
-//            monkey.transform = CGAffineTransform(translationX: translation.x, y: 0)
-//        }
     }
     
-//    @objc private func handleDirectedPanGesture(_ gesture: DirectedPanGestureRecognizer) {
-//
-//        guard let direction = gesture.direction else { return }
-//
-//        switch direction {
-//        case .right:
-//            banana.transform = CGAffineTransform(translationX: 80, y: 0)
-//        case .left:
-//            banana.transform = CGAffineTransform(translationX: -80, y: 0)
-//        default:
-//            break
-//        }
-//    }
     
+}
+extension Thread {
+    var threadName: String {
+        if isMainThread {
+            return "main"
+        } else if let threadName = Thread.current.name, !threadName.isEmpty {
+            return threadName
+        } else {
+            return description
+        }
+    }
     
+    var queueName: String {
+        if let queueName = String(validatingUTF8: __dispatch_queue_get_label(nil)) {
+            return queueName
+        } else if let operationQueueName = OperationQueue.current?.name, !operationQueueName.isEmpty {
+            return operationQueueName
+        } else if let dispatchQueueName = OperationQueue.current?.underlyingQueue?.label, !dispatchQueueName.isEmpty {
+            return dispatchQueueName
+        } else {
+            return "n/a"
+        }
+    }
 }
